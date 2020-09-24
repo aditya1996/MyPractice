51b110c4e0d3bde1da47360bb67dc6c20687846b
Aditya Tiwari <adityatiwari@Adityas-MacBook-Pro.local>
2020-09-25 01:56:24 +0530

    NLP

---
+++ NLTK/predict_sentiment.py
@@ -0,0 +1,5 @@
+import sentiment_mod as s
+
+print(s.sentiment("this movie was awesome! the acting was great, plot was wonderful and there were pythons..so yeah!"))
+
+print(s.sentiment("this movie was junk. No point to the movie and it was absolutely junk."))
